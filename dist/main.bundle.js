19b60353e0e922edc085e8c5fba55b70a17ea2d4
Claudia Mariana Rios Jasso <rios.mariana@mx1.ibm.com>
2017-09-05 14:53:10 -0500

    florantus1.0

---
+++ dist/main.bundle.js
@@ -0,0 +1,158 @@
+webpackJsonp([1],{
+
+/***/ "../../../../../src async recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "../../../../../src async recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<body>\n    <div>\n        <div class=\"header-dark\">\n            <nav class=\"navbar navbar-default navigation-clean-search\">\n                <div class=\"container\">\n                    <div class=\"navbar-header\"><a class=\"navbar-brand navbar-link\" href=\"#\">Ana Gil - Joyeria de Mexico</a>\n                        <button class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navcol-1\"><span class=\"sr-only\">Toggle navigation</span>\n                            <span class=\"icon-bar\"></span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span></button>\n                    </div>\n                    <div class=\"collapse navbar-collapse\" id=\"navcol-1\">\n                        <ul class=\"nav navbar-nav\">\n                            <li role=\"presentation\">\n                                <a href=\"#\"> </a>\n                            </li>\n                            <li class=\"dropdown\"><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" aria-expanded=\"true\" href=\"#\">Collections <span class=\"caret\"></span></a>\n                                <ul class=\"dropdown-menu\" role=\"menu\">\n                                    <li role=\"presentation\"><a href=\"#\">First Item</a></li>\n                                    <li role=\"presentation\"><a href=\"#\">Second Item</a></li>\n                                    <li role=\"presentation\"><a href=\"#\">Third Item</a></li>\n                                </ul>\n                            </li>\n                        </ul>\n                        <form class=\"navbar-form navbar-left\" target=\"_self\">\n                            <div class=\"form-group\">\n                                <label class=\"control-label\" for=\"search-field\"><i class=\"glyphicon glyphicon-search\"></i></label>\n                                <input class=\"form-control search-field\" type=\"search\" name=\"search\" id=\"search-field\">\n                            </div>\n                        </form>\n                        <p class=\"navbar-text navbar-right\"> </p>\n                    </div>\n                </div>\n            </nav>\n            <div class=\"container hero\">\n                <div class=\"row\">\n                    <div class=\"\">\n                        <h1 class=\"text-center\">FLORANTUS</h1>\n                    </div>\n                </div>\n            </div>\n        </div>\n    </div>\n    <div class=\"highlight-blue\">\n        <div class=\"container\">\n            <div class=\"intro\">\n                <h2 class=\"text-center\">Inspirada en México y su flora</h2>\n                <p class=\"text-center\">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>\n            </div>\n            <div class=\"buttons\"><a class=\"btn btn-primary\" style=\"color: black; border-color:turquoise\" role=\"button\" href=\"#\">Order Now</a></div>\n        </div>\n    </div>\n    <div style='width: 200; height: 200px'>\n    <div parallax speed=\"5\" style=\"background-attachment: fixed; width: 100%; height: 100%; margin: 0px auto; position: relative; background-image: url(assets/img/ana_gil2.jpg); \n    background-position: initial initial; background-repeat: initial initial;\"></div>\n    </div>\n    <div class=\"article-list\">\n        <div class=\"container\">\n            <div class=\"intro\">\n                <h2 class=\"text-center\">2017 Collection</h2>\n                <p class=\"text-center\" style=\"font-family: 'Dancing Script;\">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>\n            </div>\n            <div class=\"row articles\">\n                <div class=\"col-md-4 col-sm-6 item\">\n                    <a href=\"#\"><img class=\"img-responsive\" src=\"assets/img/collection.jpeg\"></a>\n                    <h3 class=\"name\">New Image</h3>\n                    <p class=\"description\">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a href=\"#\" class=\"action\"><i class=\"glyphicon glyphicon-circle-arrow-right\"></i></a></div>\n                <div\n                class=\"col-md-4 col-sm-6 item\">\n                    <a href=\"#\"><img class=\"img-responsive\" src=\"assets/img/rings.jpeg\"></a>\n                    <h3 class=\"name\">The Rings</h3>\n                    <p class=\"description\">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a href=\"#\" class=\"action\"><i class=\"glyphicon glyphicon-circle-arrow-right\"></i></a></div>\n            <div\n            class=\"col-md-4 col-sm-6 item\">\n                <a href=\"#\"><img class=\"img-responsive\" src=\"assets/img/bracelets.jpeg\"></a>\n                <h3 class=\"name\">The Bracelets</h3>\n                <p class=\"description\">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a href=\"#\" class=\"action\"><i class=\"glyphicon glyphicon-circle-arrow-right\"></i></a></div>\n    </div>\n    </div>\n    </div>\n    <div class=\"footer-basic\">\n        <footer>\n            <div class=\"social\"><a href=\"#\"><i class=\"icon ion-social-instagram\"></i></a><a href=\"#\"><i class=\"icon ion-social-snapchat\"></i></a><a href=\"#\"><i class=\"icon ion-social-twitter\"></i></a><a href=\"#\"><i class=\"icon ion-social-facebook\"></i></a></div>\n            <ul class=\"list-inline\">\n                <li><a href=\"#\">Home</a></li>\n                <li><a href=\"#\">Services</a></li>\n                <li><a href=\"#\">About</a></li>\n                <li><a href=\"#\">Terms</a></li>\n                <li><a href=\"#\">Privacy Policy</a></li>\n            </ul>\n            <p class=\"copyright\">Ana Gil - Joyería de México © 2017</p>\n        </footer>\n    </div>\n    <div></div>\n    <script src=\"assets/js/jquery.min.js\"></script>\n    <script src=\"assets/bootstrap/js/bootstrap.min.js\"></script>\n    <script src=\"assets/js/bs-animation.js\"></script>\n    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js\"></script>\n</body>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
